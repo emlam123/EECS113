67927ec51b94c4e0a6dfd935ff2a4e28b66aa82a
kiyoungbang <51379037+kiyoungbang@users.noreply.github.com>
2019-06-04 11:42:00 -0700

    Update relay_motion.py
    added comments

--- relay_motion.py
+++ relay_motion.py
@@ -1,29 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 
+#GPIO pins defined
 RelayPin = 17
 PIRPin = 26
-def setup():
-	GPIO.setwarnings(False)
+
+def setup():	#pin and board setup
+	GPIO.setwarnings(False)			
 	GPIO.setmode(GPIO.BCM)
-	GPIO.setup(RelayPin, GPIO.OUT, initial = GPIO.LOW)
-	GPIO.setup(PIRPin, GPIO.IN)
+	GPIO.setup(RelayPin, GPIO.OUT, initial = GPIO.LOW)	#relay is closed = sprinkler off	
+	GPIO.setup(PIRPin, GPIO.IN)				#motion sensor set as an input
 	GPIO.setup()
 def main():
 	while True:
-		detect = GPIO.input(PIRPin)
-		if detect == 1:
+		detect = GPIO.input(PIRPin)	#motion sensor is always detecting motion
+		if detect == 1:			#if motion is detected
 			print('Relay close: Sprinkler Off')
 			print('Motion detected')
-			GPIO.output(RelayPin, GPIO.LOW)
+			GPIO.output(RelayPin, GPIO.LOW)		#close the relay to turn off the sprinkler
 			
-		elif detect == 0:
-			print('Relay open: Sprinkler On')
+		elif detect == 0:		#if no motion is detected
+			print('Relay open: Sprinkler On')	
 			print('No motion')
-			GPIO.output(RelayPin, GPIO.HIGH)
+			GPIO.output(RelayPin, GPIO.HIGH)	#keep the relay open
 			time.sleep(1)
 
-def destroy():
+def destroy():	#clean up function when everything is done
 	GPIO.output(RelayPin, GPIO.LOW)
 	GPIO.cleanup()
 	
